569bf39e6c71db7ad3ac1fd457a33618cd744c1e
David-Haley <34273510+David-Haley@users.noreply.github.com>
2022-12-26 23:52:41 +1000

    Day 24 Solution
    Part two execution for my data was a bit long (52 s). The simple breadth first search, with the heuristic of ensuring that repeats of the same state are not inserted back into the Queue.

--- src/december_24.adb
+++ src/december_24.adb
@@ -7,7 +7,7 @@ with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Vectors;
 with Ada.Containers.Ordered_Sets;
 with Ada.Containers.Synchronized_Queue_Interfaces;
-with Ada.Containers.Unbounded_Priority_Queues;
+with Ada.Containers.Unbounded_Synchronized_Queues;
 with DJH.Execution_Time; use DJH.Execution_Time;
 
 procedure December_24 is
@@ -41,30 +41,23 @@ procedure December_24 is
    type State_Element is record
       Time : Times;
       Position : Coordinates;
-      Distance : Distances;
    end record; -- State_Element
+
+   function "<" (Left, Right : State_Element) return Boolean is
+     (Left.Time < Right.Time or
+        (Left.Time = Right.Time and Left.Position < Right.Position));
+
+   function "=" (Left, Right : State_Element) return Boolean is
+     (Left.Time = Right.Time and Left.Position = Right.Position);
+
+   package State_Sets is new Ada.Containers.Ordered_Sets (State_Element);
+   use State_Sets;
 
    package Queue_Interface is new
      Ada.Containers.Synchronized_Queue_Interfaces (State_Element);
 
-   type Priority is record
-      Time : Times;
-      Distance : Distances;
-   end record; -- Priority
-
-   function Get_Priority (Element : State_Element) return Priority is
-     (Element.Time, Element.Distance);
-
-   function Shorter_First (Left, Right : Priority) return Boolean is
-     (Left.Time < Right.Time or
-        (Left.Time = Right.Time and Left.Distance < Right.Distance));
-
-   package Path_Queues is new Ada.Containers.Unbounded_Priority_Queues
-     (Queue_Interfaces => Queue_Interface,
-      Queue_Priority => Priority,
-      Get_Priority => Get_Priority,
-      Before => Shorter_First);
-   use Path_Queues;
+   package Path_Queues is new
+     Ada.Containers.Unbounded_Synchronized_Queues (Queue_Interface);
    use Path_Queues;
 
    procedure Read_Input (Wall : out Coordinate_Sets.Set;
@@ -121,52 +114,17 @@ procedure December_24 is
          raise;
    end Read_Input;
 
-   procedure Put (Wall : in Coordinate_Sets.Set;
-                  Blizzard_Store : in Blizzard_Stores.Vector;
-                  Current_Element : in State_Element) is
-
-      Combined : Coordinate_Sets.Set := Coordinate_Sets.Empty_Set;
-
-   begin -- Put
-      Put_Line (Current_Element.Time'img & Current_Element.Distance'Img);
-      Combined := Blizzard_Store (Current_Element.Time) (North) or
-        Blizzard_Store (Current_Element.Time) (South) or
-        Blizzard_Store (Current_Element.Time) (East) or
-        Blizzard_Store (Current_Element.Time) (West);
-      for Y in Ordinates range 1 .. Last_Element (Wall).Y loop
-         for X in Ordinates range 1 .. Last_Element (Wall).X loop
-            if Contains (Wall, (X, Y)) then
-               Put ('#');
-            elsif Contains (Combined, (X, Y)) then
-               if (X, Y) = Current_Element.Position then
-                  Put ('?');
-               else
-                  Put ('B');
-               end if;
-            elsif (X, Y) = Current_Element.Position then
-               Put ('E');
-            else
-               Put ('.');
-            end if;
-         end loop;
-         New_Line;
-      end loop;
-      New_Line;
-   end Put;
-
    function Find_Path (Wall : in Coordinate_Sets.Set;
                        Blizzard_Store : in out Blizzard_Stores.Vector;
-                       Start, Finish :  in Coordinates) return Times is
+                       Start : in State_Element;
+                       Finish :  in Coordinates) return Times is
 
       subtype Deltas is Integer range -1 .. 1;
-
-      Function Man_Distance (P1, P2 : in Coordinates) return Distances is
-        (abs (P1.X - P2.X) + abs (P1.Y - P2.Y));
 
       function is_Clear (Blizzard_Store : in out Blizzard_Stores.Vector;
                          Wall : in Coordinate_Sets.Set;
                          Position : in Coordinates; Time : in Times)
-                            return Boolean is
+                         return Boolean is
 
          procedure Update (Blizzard_Store : in out Blizzard_Stores.Vector;
                            Wall : in Coordinate_Sets.Set ) is
@@ -219,10 +177,9 @@ procedure December_24 is
          pragma Inline_Always (Update);
 
       begin -- is_Clear
-         while Time > Last_Index (Blizzard_Store) loop
+         if Time > Last_Index (Blizzard_Store) then
             Update (Blizzard_Store, Wall);
-            Put_Line ("Update:" & Time'Img);
-         end loop; -- Time > Last_Index (Blizzard_Store)
+         end if; -- Time > Last_Index (Blizzard_Store)
          return not (Contains (Blizzard_Store (Time) (North), Position) or
                        Contains (Blizzard_Store (Time) (South), Position) or
                          Contains (Blizzard_Store (Time) (East), Position) or
@@ -236,62 +193,57 @@ procedure December_24 is
                           Blizzard_Store : in out Blizzard_Stores.Vector;
                           Wall : in Coordinate_Sets.Set;
                           Finish : in Coordinates;
+                          State_Set : in out State_Sets.Set;
                           Xd, Yd : in Deltas;
                           Next_Element : out State_Element) return Boolean is
 
          Is_Valid : Boolean;
+         X : Integer := Current_Element.Position.X + Xd;
+         Y : Integer := Current_Element.Position.Y + Yd;
 
       begin -- Next_Step
-         Is_Valid := (Current_Element.Position.X + Xd in Ordinates and
-                        Current_Element.Position.Y + Yd in Ordinates) and then
-           Is_Clear (Blizzard_Store, Wall,
-                     (Current_Element.Position.X + Xd,
-                      Current_Element.Position.Y + Yd),
-                     Current_Element.Time + 1);
+         Is_Valid := (X in Ordinates and Y in Ordinates) and then
+           not Contains (State_Set, (Current_Element.Time + 1, (X, Y))) and then
+           Is_Clear (Blizzard_Store, Wall, (X, Y), Current_Element.Time + 1);
          if Is_Valid then
             Next_Element.Time := Current_Element.Time + 1;
-            Next_Element.Position :=
-              (Current_Element.Position.X + Xd, Current_Element.Position.Y + Yd);
-            Next_Element.Distance :=
-              Man_Distance ((Current_Element.Position.X + Xd,
-                            Current_Element.Position.Y + Yd), Finish);
+            Next_Element.Position := (X, Y);
+            Include (State_Set, Next_Element);
          end if; -- Is_Valid
          return Is_Valid;
       end Next_Step;
 
       Current_Element, Next_element : State_Element;
       Path_Queue : Path_Queues.Queue;
+      State_Set : State_Sets.Set := State_Sets.Empty_Set;
 
       pragma Inline_Always (Next_Step);
 
-
    begin -- Find_Path
-      Current_Element := (Time => 0,
-                          Position => Start,
-                          Distance => Man_Distance (Finish, Start));
+      Current_Element := Start;
       Path_Queue.Enqueue (Current_Element);
       while Path_Queue.Current_Use > 0 and
         Current_Element.Position /= Finish loop
          Path_Queue.Dequeue (Current_Element);
          -- Put (Wall, Blizzard_Store, Current_Element);
-         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, 1, 0,
-                       Next_Element) then
+         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, State_Set,
+                       1, 0, Next_Element) then
             Path_Queue.Enqueue (Next_Element);
          end if; -- East
-         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, 0, 1,
-                       Next_Element) then
+         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, State_Set,
+                       0, 1, Next_Element) then
             Path_Queue.Enqueue (Next_Element);
          end if; -- South
-         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, -1, 0,
-                       Next_Element) then
+         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, State_Set,
+                       -1, 0, Next_Element) then
             Path_Queue.Enqueue (Next_Element);
          end if; -- West
-         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, 0, -1,
-                       Next_Element) then
+         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, State_Set,
+                       0, -1, Next_Element) then
             Path_Queue.Enqueue (Next_Element);
          end if; -- North
-         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, 0, 0,
-                       Next_Element) then
+         if Next_Step (Current_Element, Blizzard_Store, Wall, Finish, State_Set,
+                       0, 0, Next_Element) then
             Path_Queue.Enqueue (Next_Element);
          end if; -- Stay put
       end loop;
@@ -305,12 +257,19 @@ procedure December_24 is
    Wall : Coordinate_Sets.Set;
    Blizzard_Store : Blizzard_Stores.Vector;
    Start, Finish : Coordinates;
+   Start_State : State_Element;
+   End_Time : Times;
 
 begin -- December_24
    Read_Input (Wall, Blizzard_Store, Start, Finish);
-   Put_Line ("Part one:" &
-               Find_Path (Wall, Blizzard_Store, Start, Finish)'Img);
+   Start_State := (Time => 0, Position => Start);
+   End_Time := Find_Path (Wall, Blizzard_Store, Start_State, Finish);
+   Put_Line ("Part one:" & End_Time'Img);
    DJH.Execution_Time.Put_CPU_Time;
-   Put_Line ("Part two:");
+   Start_State := (Time => End_Time, Position => Finish);
+   End_Time := Find_Path (Wall, Blizzard_Store, Start_State, Start);
+   Start_State := (Time => End_Time, Position => Start);
+   End_Time := Find_Path (Wall, Blizzard_Store, Start_State, Finish);
+   Put_Line ("Part two:" & End_Time'Img);
    DJH.Execution_Time.Put_CPU_Time;
 end December_24;
